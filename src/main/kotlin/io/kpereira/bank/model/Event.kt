db8163bdc23e37cd6da3ca940b34a8f1de0eb723
Kaue Pereira <kauefontes@outlook.com>
2021-01-18 17:43:49 -0400

    Add nullable to all attributes

--- src/main/kotlin/io/kpereira/bank/model/Event.kt
+++ src/main/kotlin/io/kpereira/bank/model/Event.kt
@@ -1,8 +1,8 @@
 package io.kpereira.bank.model
 
 class Event(
-    val type: String,
-    val origin: String,
-    val destination: String,
-    val amount: Int,
+    val type: String?,
+    val origin: String?,
+    val destination: String?,
+    val amount: Int?,
 )
