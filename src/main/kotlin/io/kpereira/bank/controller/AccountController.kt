f52eb3cffef16c3e965dcb3d8bbd0ce7eb0cefb6
Kaue Pereira <kauefontes@outlook.com>
2021-01-18 17:54:20 -0400

    Added event request and renamed getBalance() to balance()

--- src/main/kotlin/io/kpereira/bank/controller/AccountController.kt
+++ src/main/kotlin/io/kpereira/bank/controller/AccountController.kt
@@ -2,8 +2,9 @@ package io.kpereira.bank.controller
 
 import io.kpereira.bank.model.Event
 import io.kpereira.bank.service.AccountService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class AccountController(private val accountService: AccountService) {
 
     @RequestMapping(value = ["/balance"], method = [RequestMethod.GET])
-    fun getBalance(account_id: Long): ResponseEntity<Int> {
+    fun balance(account_id: Long): ResponseEntity<Int> {
         if (accountService.exists(account_id))
             return ResponseEntity.ok(accountService.getBalance(account_id))
         return ResponseEntity.notFound().build()
@@ -22,5 +23,16 @@ class AccountController(private val accountService: AccountService) {
     fun reset(): ResponseEntity<Unit> {
         accountService.clearAllAccounts()
         return ResponseEntity.ok().build()
+    }
+
+    @RequestMapping(value = ["/event"], method = [RequestMethod.POST])
+    fun event(@RequestBody event: Event): ResponseEntity<Any> {
+        val origin = event.origin!!.toLong()
+        if ((event.type == "transfer" && !accountService.exists(origin))
+            || (event.type == "withdraw" && !accountService.exists(origin))
+        ) {
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity(accountService.delegateEvents(event), HttpStatus.CREATED)
     }
 }
