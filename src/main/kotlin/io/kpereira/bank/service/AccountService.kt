7672b6145faf5fede3dd478d0d77bb8d2c18f32e
Kaue Pereira <kauefontes@outlook.com>
2021-01-18 01:50:47 -0400

    Deposit event created

--- src/main/kotlin/io/kpereira/bank/service/AccountService.kt
+++ src/main/kotlin/io/kpereira/bank/service/AccountService.kt
@@ -1,10 +1,22 @@
 package io.kpereira.bank.service
 
+import io.kpereira.bank.model.Account
+import io.kpereira.bank.model.Event
 import io.kpereira.bank.repository.AccountRepository
 import org.springframework.stereotype.Service
 
 @Service
 class AccountService(private val accountRepository: AccountRepository) {
+
+    fun delegateEvent(event: Event) {
+        when (event.type) {
+            "deposit" -> deposit(Account(event.destination.toLong(), event.amount))
+        }
+    }
+
+    fun deposit(account: Account): Account {
+        return accountRepository.save(account)
+    }
 
     fun reset() {
         accountRepository.deleteAll()
