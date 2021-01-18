717c74d8fda84cde78133650e5670b5343683848
Kaue Pereira <kauefontes@outlook.com>
2021-01-18 01:11:42 -0400

    Creating method to reset data

--- src/main/kotlin/io/kpereira/bank/service/AccountService.kt
+++ src/main/kotlin/io/kpereira/bank/service/AccountService.kt
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Service
 
 @Service
 class AccountService(private val accountRepository: AccountRepository) {
+
+    fun reset() {
+        accountRepository.deleteAll()
+    }
 }
