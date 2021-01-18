2dd0d741f4506c1859c01dd44c8e24f380a2c34d
Kaue Pereira <kauefontes@outlook.com>
2021-01-18 02:17:49 -0400

    Added logic to deposit method, added method to get balance and two utils methods

--- src/main/kotlin/io/kpereira/bank/service/AccountService.kt
+++ src/main/kotlin/io/kpereira/bank/service/AccountService.kt
@@ -4,6 +4,7 @@ import io.kpereira.bank.model.Account
 import io.kpereira.bank.model.Event
 import io.kpereira.bank.repository.AccountRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class AccountService(private val accountRepository: AccountRepository) {
@@ -15,10 +16,30 @@ class AccountService(private val accountRepository: AccountRepository) {
     }
 
     fun deposit(account: Account): Account {
+        val id = account.id
+        if (exist(id)) {
+            val balance = findById(id).balance
+            val value = account.balance
+            val newBalance = balance + value
+            return accountRepository.save(Account(id, newBalance))
+        }
         return accountRepository.save(account)
+    }
+
+    fun findById(id: Long): Account {
+        return accountRepository.findById(id).get()
+    }
+
+    fun balance(id: Long): Int {
+        val account = accountRepository.findById(id).get()
+        return account.balance
     }
 
     fun reset() {
         accountRepository.deleteAll()
+    }
+
+    fun exist(id: Long): Boolean {
+        return accountRepository.existsById(id)
     }
 }
